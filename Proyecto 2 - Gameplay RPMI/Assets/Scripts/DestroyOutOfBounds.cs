439cb7a2e74b80aad141439746caaa0150f9f93f
msantos98 <maria@frogames.es>
2021-11-22 16:06:13 +0100

    22 nov

---
+++ Proyecto 2 - Gameplay RPMI/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float upperLim = 30f;
+    private float lowerLim = -10f;
+    
+    void Update()
+    {
+        // Bala fallida
+        if (transform.position.z > upperLim)
+        {
+            Destroy(gameObject);
+        }
+        
+        // Animal cruza el límite inferior
+        if (transform.position.z < lowerLim)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
--- Proyecto 2 - Gameplay RPMI/Assets/Scripts/PlayerController.cs
+++ Proyecto 2 - Gameplay RPMI/Assets/Scripts/PlayerController.cs
@@ -6,24 +6,39 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10f;
-    public float xRange = 16;
+    public float xRange = 16f;
+
+    public GameObject projectilePrefab;
     
-    // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput);
 
+        // Límite de pantalla derecho
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y,
                 transform.position.z);
+        }
+        
+        // Límite de pantalla izquierdo
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y,
+                transform.position.z);
+        }
+        
+        // Disparo
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position,
+                projectilePrefab.transform.rotation);
         }
     }
 }
