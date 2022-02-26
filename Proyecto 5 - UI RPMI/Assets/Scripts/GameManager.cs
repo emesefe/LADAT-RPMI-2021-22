1a3ba443b9872329c1ebe59a5b2519766d39b38c
msantos98 <maria@frogames.es>
2022-02-26 18:14:57 +0100

    cambios

--- Proyecto 5 - UI RPMI/Assets/Scripts/GameManager.cs
+++ Proyecto 5 - UI RPMI/Assets/Scripts/GameManager.cs
@@ -1,37 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject[] targetPrefabs;
     public bool isGameOver;
-    public List<Vector3> targetPositions;
+    public List<Vector3> targetPositions; // Guarda las posiciones ocupadas
 
+    public TextMeshProUGUI scoreText;
+    public GameObject gameOverPanel;
+
+    // Coordenadas del cuadrado inferior izquierdo
     private float minX = -3.75f;
     private float minY = -3.75f;
+    // Distancia entre los centros de los cuadrados (horizontal y vertical)
     private float distanceBetweenSquares = 2.5f;
 
     private float spawnRate = 2f;
     private Vector3 randomPos;
+
+    private int score = 0;
     
     void Start()
     {
+        scoreText.text = $"Score: {score}";
+        gameOverPanel.SetActive(false);
         StartCoroutine(SpawnRandomTarget());
-    }
-
-
-    void Update()
-    {
-        
     }
 
     private Vector3 RandomSpawnPosition()
     {
+        // Función que calcula una posición aleatoria en los centros de los 16 cuadrados
+        
         int SaltosX = Random.Range(0, 4);
         int SaltosY = Random.Range(0, 4);
 
+        // Partimos desde el cuadrado inferior izquierdo
+        // Calculamos el desplazamiento hacia la derecha
         float spawnPosX = minX + SaltosX * distanceBetweenSquares;
+        // Calculamos el desplazamiento hacia arriba
         float spawnPosY = minY + SaltosY * distanceBetweenSquares;
         
         return new Vector3(spawnPosX, spawnPosY, 0);
@@ -41,17 +51,42 @@ public class GameManager : MonoBehaviour
     {
         while (!isGameOver)
         {
+            // Esperamos spawnRate segundos
             yield return new WaitForSeconds(spawnRate);
+            
+            // Elegimos de forma aleatoria el target que va a aparecer
             int randomIndex = Random.Range(0, targetPrefabs.Length);
+            
+            // Calculamos la posición aleatoria
             randomPos = RandomSpawnPosition();
+            // Comprobamos que no está ocupada (solo hay un total de 16 disponibles)
             while (targetPositions.Contains(randomPos))
             {
                 randomPos = RandomSpawnPosition();
             }
 
+            // Instanciamos el objeto
             Instantiate(targetPrefabs[randomIndex], randomPos,
                 targetPrefabs[randomIndex].transform.rotation);
+            // Añadimos su posición a la lista de posiciones ocupadas
             targetPositions.Add(randomPos);
         }
+    }
+
+    public void UpdateScore(int pointToAdd)
+    {
+        score += pointToAdd;
+        scoreText.text = $"Score: {score}";
+    }
+
+    public void GameOver()
+    {
+        isGameOver = true;
+        gameOverPanel.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Proyecto 5 - UI RPMI/Assets/Scripts/Target.cs
+++ Proyecto 5 - UI RPMI/Assets/Scripts/Target.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    public int points;
+    public ParticleSystem explosionParticle;
+    
     private float lifeTime = 2f;
 
     private GameManager gameManagerScript;
@@ -17,16 +20,26 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Destroy(gameObject);
-
-        if (gameObject.CompareTag("Bad"))
+        if (!gameManagerScript.isGameOver)
         {
-            gameManagerScript.isGameOver = true;
+            gameManagerScript.UpdateScore(points);
+            Instantiate(explosionParticle, transform.position, 
+                explosionParticle.transform.rotation);
+
+            // Destruimos el target si hacemos click sobre éste
+            Destroy(gameObject);
+
+            // Si le damos a un target malo (calavera), Game Over
+            if (gameObject.CompareTag("Bad"))
+            {
+                gameManagerScript.GameOver();
+            }
         }
     }
 
     private void OnDestroy()
     {
+        // Cuando se destruye el target, comunicamos a Game Manager que la posición queda libre
         gameManagerScript.targetPositions.Remove(transform.position);
     }
 }
